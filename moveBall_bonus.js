53a562bf8bb7f9e48e7821e2581ed1229bdfc150
Camilo Rincon <123512032+carin91@users.noreply.github.com>
2023-02-03 02:31:07 -0500

    Add files via upload

--- moveBall_bonus.js
+++ moveBall_bonus.js
@@ -1,26 +1,39 @@
 
 
-
-var velocity = 5;
-var position = 0;
+var velocityX = 5;
+var velocityY = 3;
+var positionX = 0;
+var positionY = 0;
 var ball = document.getElementById('ball');
 
 // YOUR CODE 
 // ----------------
+
 function mover(){
-  if(position>300){
+  if(positionX>500){
     //clearInterval(intervalo1);
     //var intervalo2=setInterval(mover,100,-5);
-    velocity=-5;
+    velocityX=-5;
   }
-  if(position<0){
+  if(positionX<0){
    // clearInterval(intervalo1);
     //var intervalo1=setInterval(mover,100,5);
-    velocity=5;
+    velocityX=5;
   }
-  ball.style.left=position+"px";
-  console.log("la posicion es",position)
-  position=position+velocity;
+  if(positionY>500){
+    //clearInterval(intervalo1);
+    //var intervalo2=setInterval(mover,100,-5);
+    velocityY=-3;
+  }
+  if(positionY<0){
+   // clearInterval(intervalo1);
+    //var intervalo1=setInterval(mover,100,5);
+    velocityY=3;
+  }
+  ball.style.left=positionX+"px";
+  ball.style.top=positionY+"px";
+  positionX=positionX+velocityX;
+  positionY=positionY+velocityY;
   
   
 }
